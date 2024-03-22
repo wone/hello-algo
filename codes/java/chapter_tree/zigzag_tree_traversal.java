a360ffbbf607b017e1ccdc40ad4f8f332fcdc05f
ivenzhang <ivenzhang@tencent.com>
2024-03-22 19:32:30 +0800

    zigzag tree traversal

--- codes/java/chapter_tree/zigzag_tree_traversal.java
+++ codes/java/chapter_tree/zigzag_tree_traversal.java
@@ -8,7 +8,7 @@ import java.util.*;
 /**
  * 广度优先遍历
  */
-public class my_tree_test {
+public class zigzag_tree_traversal {
     public static void main(String[] args) {
         /* 初始化二叉树 */
         // 这里借助了一个从数组直接生成二叉树的函数
@@ -50,24 +50,30 @@ public class my_tree_test {
         }
     }
 
-//    public static void printTreeByZigzagOrder(TreeNode root) {
-//        Queue<TreeNode> queue = new LinkedList<>();
-//        queue.add(root);
-//
-//        while (!queue.isEmpty()) {
-//            TreeNode node = (TreeNode) queue.poll();
-//            System.out.println(node.val);
-//
-//            if (node.left != null) {
-//                queue.add(node.left);
-//            }
-//
-//            if (node.right != null) {
-//                queue.add(node.right);
-//            }
-//        }
-//    }
-
+    
+    /**
+     * Zigzag遍历（也称之为蛇形遍历）是一种特殊的树遍历方式，它按层遍历树的节点，但在每一层上，节点的访问顺序会发生变化。在奇数层（从根节点开始计数），我们按从左到右的顺序访问节点；在偶数层，我们按从右到左的顺序访问节点。
+     *
+     * 给定一个完全二叉树，我们可以使用以下解题思路来实现Zigzag遍历：
+     *
+     * 1. 使用两个栈（stack1和stack2）来分别存储当前层和下一层的节点。初始时，将根节点压入stack1。
+     *
+     * 2. 使用一个布尔变量`left_to_right`来表示当前层的遍历顺序。对于奇数层，`left_to_right`为`true`；对于偶数层，`left_to_right`为`false`。初始时，将`left_to_right`设置为`true`。
+     *
+     * 3. 当stack1不为空时，执行以下步骤：
+     *
+     *    a. 弹出stack1的顶部节点，并访问该节点。
+     *
+     *    b. 如果`left_to_right`为`true`，则首先将该节点的左子节点压入stack2，然后将右子节点压入stack2。如果`left_to_right`为`false`，则首先将该节点的右子节点压入stack2，然后将左子节点压入stack2。
+     *
+     *    c. 如果stack1为空（即已访问完当前层的所有节点），则交换stack1和stack2（即将下一层的节点设置为当前层），并将`left_to_right`的值取反。
+     *
+     * 4. 重复步骤3，直到stack1为空且stack2为空（即已访问完所有节点）。
+     *
+     * 这种解题思路的关键在于使用两个栈来分别存储当前层和下一层的节点，并根据`left_to_right`变量来确定在每一层上访问节点的顺序。通过交替地改变遍历顺序，我们可以实现Zigzag遍历。
+     * @param root
+     * @return
+     */
     public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) {
